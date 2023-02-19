fa73dfa7501b830b31b2a385aea6306398b82383
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-02-19 12:36:12 +0000

    test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Solution sol = new Solution();
-sol.mul
+sol.Multiply("2","3");
 
 
 public class Solution {
     public string Multiply(string num1, string num2) {
-        
+
         return null;
     }
 }
