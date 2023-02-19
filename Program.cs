5b75d95b70561114fa812a9f883e9aaa1a7897ac
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-02-19 12:35:56 +0000

    saving code

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Solution sol = new Solution();
+sol.mul
+
+
+public class Solution {
+    public string Multiply(string num1, string num2) {
+        
+        return null;
+    }
+}
